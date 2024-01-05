e501beb15e0b4f68f003dcc1a04fe05c374b7c79
isomorphicdude <iamthesenate@qq.com>
2024-01-05 17:05:26 +0000

    Refactor compile_pdf.sh script to improve compilation process

--- compile_pdf.sh
+++ compile_pdf.sh
@@ -10,20 +10,20 @@ fi
 mkdir -p output
 
 # Compile 'main.tex' to PDF using pdflatex (you can change the compiler if needed)
-latexmk -synctex=1 -interaction=nonstopmode -file-line-error -pdf -outdir=output main.tex
+latexmk -synctex=1 -interaction=nonstopmode -file-line-error -pdf main.tex
 
 
 # Check if compilation was successful
 if [ $? -eq 0 ]; then
-    echo "Compilation successful. PDF saved in 'output' directory."
+    echo "Compilation successful. PDF saved."
 
     # # Clean up unnecessary files (log files, auxiliary files)
     # find . -type f -regex '.*\.\(aux\|log\|toc\|out\|gz\|bbl\|blg\|snm\|nav\|vrb\|fls\|fdb_latexmk\|synctex.gz\)' -exec rm {} \;
     # run py script to clean up unnecessary files
-    python remove_aux.py
+    python remove_aux.py --not_remove_pdf
 else
     echo "Compilation failed. Check for LaTeX errors."
     # clean everything except log files
-    python remove_aux.py --debug
+    python remove_aux.py --debug --not_remove_pdf
 fi
 
